a69201eaa063830af407cd92b128393986003c11
A K A <66775180+vari98@users.noreply.github.com>
2020-12-17 22:38:21 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -102,7 +102,7 @@ def get_readable_message():
                 if hasattr(download, 'is_torrent'):
                     msg += f"| P: {download.aria_download().connections} " \
                            f"| S: {download.aria_download().num_seeders}"
-                msg += f"\nGID: <code>{download.gid()}</code>"
+                msg += f"\n<code>/cancel {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
